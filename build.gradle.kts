c222f294740bd665ec9577efb09e26c711df11cf
Jakub Ziec <jakziec@gmail.com>
2023-11-10 11:10:13 +0100

    Project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+	id("groovy")
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
---
+++ src/test/java/com/smarthost/SmartHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
