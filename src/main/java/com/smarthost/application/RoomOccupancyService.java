1beef4c0c77cf5d976174a579b1cef523cfca14a
Jakub Ziec <jakziec@gmail.com>
2023-11-23 16:06:03 +0100

    Refactor calcRoomsOccupancy - add GuestOffersByCategory record

--- src/main/java/com/smarthost/application/RoomOccupancyService.java
+++ src/main/java/com/smarthost/application/RoomOccupancyService.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.lang.Math.min;
@@ -24,12 +25,10 @@ public class RoomOccupancyService {
     }
 
     public RoomsOccupancy calcRoomsOccupancy(AvailableRoomsCount availableRoomsCount) {
-        final var guestOffersByCategory = guestOfferRepository.findAllGuestOffers().stream()
-                .sorted(Comparator.comparing(GuestOffer::amount).reversed())
-                .collect(Collectors.partitioningBy(this::isOfferPremiumPriced));
-        final var takenPremiumOffers = guestOffersByCategory.get(true).stream().limit(availableRoomsCount.premium())
+        final var guestOffersByCategory = partitionOffersByCategory(guestOfferRepository.findAllGuestOffers());
+        final var takenPremiumOffers = guestOffersByCategory.premium().stream().limit(availableRoomsCount.premium())
                 .collect(Collectors.toCollection(ArrayList::new));
-        final var economyOffers = guestOffersByCategory.get(false);
+        final var economyOffers = guestOffersByCategory.economy();
         final var availablePremiumRoomsCount = availableRoomsCount.premium() - takenPremiumOffers.size();
         if (availablePremiumRoomsCount > 0
                 && isMoreEconomyOffersThanRoomsAvailable(availableRoomsCount, economyOffers.size())) {
@@ -58,6 +57,19 @@ public class RoomOccupancyService {
                 offers.size(),
                 offers.stream().map(GuestOffer::amount).reduce(BigDecimal.ZERO, BigDecimal::add)
         );
+    }
+
+    private record GuestOffersByCategory(
+            List<GuestOffer> premium,
+            List<GuestOffer> economy
+
+    ) {}
+
+    private GuestOffersByCategory partitionOffersByCategory(List<GuestOffer> offers) {
+        final var guestOffersByCategory = offers.stream()
+                .sorted(Comparator.comparing(GuestOffer::amount).reversed())
+                .collect(Collectors.partitioningBy(this::isOfferPremiumPriced));
+        return new GuestOffersByCategory(guestOffersByCategory.get(true), guestOffersByCategory.get(false));
     }
 
 }
